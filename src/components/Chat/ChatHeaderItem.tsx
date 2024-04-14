e2a1ca5c1ae77f8263037681f89d533b5a95ea19
amelie-schlueter <136457656+amelie-schlueter@users.noreply.github.com>
2024-04-14 18:39:41 +0200

    Update: color

--- src/components/Chat/ChatHeaderItem.tsx
+++ src/components/Chat/ChatHeaderItem.tsx
@@ -9,7 +9,7 @@ const ChatHeaderItem = ({
 }) => {
     return (
         <div className="grid grid-cols-2 border-t p-2">
-            <p className="text-orange-500  font-semibold">{label}:</p>
+            <p className="text-primary font-semibold">{label}:</p>
             <p className="text-right">{value.toString()}</p>
         </div>
     )
